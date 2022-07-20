3c7f22d09ef1e52206eb49b472fddf3a17f80380
Timothy Small <smalls.online@icloud.com>
2022-07-19 21:02:58 -0400

    Removed ASP.NET Core libraries

--- services/_Imports.cs
+++ services/_Imports.cs
@@ -4,8 +4,6 @@ global using System.Net;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using System.Threading.Tasks;
-global using Microsoft.Extensions.Hosting;
-global using Microsoft.Extensions.Logging;
 global using SmallsOnline.Web.Lib.Models.Blog;
 global using SmallsOnline.Web.Lib.Models.FavoritesOf.Albums;
 global using SmallsOnline.Web.Lib.Models.FavoritesOf.Tracks;
