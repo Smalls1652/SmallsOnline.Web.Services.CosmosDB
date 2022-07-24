f4b1bf90f530bec226dd156927eb963e882ce11a
Timothy Small <smalls.online@icloud.com>
2022-07-23 20:48:56 -0400

    Changed to get with blogUrlId before the ID

--- services/blog/CosmosDBService_GetBlogEntry.cs
+++ services/blog/CosmosDBService_GetBlogEntry.cs
@@ -15,17 +15,35 @@ public partial class CosmosDbService : ICosmosDbService
         // Get the container where the blog entries are stored.
         Container container = cosmosDbClient.GetContainer(_containerName, "blogs");
 
-        // Get the blog entry for the supplied ID.
-        BlogEntry retrievedItem = await container.ReadItemAsync<BlogEntry>(
-            id: id,
-            partitionKey: new("blog-entry")
-        );
+        BlogEntry? retrievedItem = null;
 
-        // If the content is not null,
-        // then remove the '<!--more-->' tag from the content.
-        if (retrievedItem.Content is not null)
+        QueryDefinition query = new($"SELECT * FROM c WHERE c.partitionKey = \"blog-entry\" AND c.blogUrlId =\"{id}\"");
+
+        // Execute the Cosmos DB SQL query and get the results.
+        FeedIterator<BlogEntry> containerQueryIterator = container.GetItemQueryIterator<BlogEntry>(query);
+        while (containerQueryIterator.HasMoreResults)
         {
-            retrievedItem.Content = retrievedItem.Content.Replace("<!--more-->", "");
+            // Loop through each database entry retrieved.
+            foreach (BlogEntry item in await containerQueryIterator.ReadNextAsync())
+            {
+                retrievedItem = item;
+            }
+        }
+
+        if (retrievedItem is null)
+        {
+            // Get the blog entry for the supplied ID.
+            retrievedItem = await container.ReadItemAsync<BlogEntry>(
+                id: id,
+                partitionKey: new("blog-entry")
+            );
+
+            // If the content is not null,
+            // then remove the '<!--more-->' tag from the content.
+            if (retrievedItem.Content is not null)
+            {
+                retrievedItem.Content = retrievedItem.Content.Replace("<!--more-->", "");
+            }
         }
 
         return retrievedItem;
